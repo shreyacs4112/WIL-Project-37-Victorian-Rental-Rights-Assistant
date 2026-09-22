61bf72b74ce18da23994da671db05bee66543585
Santhosh nagesh shashikala <santhosh7.ns@gmail.com>
2026-09-22 19:49:19 +1000

    Fix indentation for appending failed questions

--- retrieval/eval_retrieval.py
+++ retrieval/eval_retrieval.py
@@ -51,8 +51,8 @@ def evaluate(retriever, test_set, k_values=(1, 3, 5)):
             scores[k].append(ndcg_at_k(ranked_ids, relevant_ids, k))
             hit_scores[k].append(hit_at_k(ranked_ids, relevant_ids, k))
 
-        if hit_scores[k][-1] == 0:
-            failed_questions[k].append(item["question_id"])
+            if hit_scores[k][-1] == 0:
+                failed_questions[k].append(item["question_id"])
     return (
     {k: sum(v) / len(v) for k, v in scores.items()},
     {k: sum(v) / len(v) for k, v in hit_scores.items()},
